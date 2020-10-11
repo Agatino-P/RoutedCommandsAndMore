dda38481fe85697b126e6cb75432d171c23b41ec
Agatino-P <57068089+Agatino-P@users.noreply.github.com>
2020-10-11 14:32:44 +0200

    Add project files.

---
+++ RoutedCommandsAndMorePrj/MainWindowViewModel.cs
@@ -0,0 +1,24 @@
+﻿using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RoutedCommandsAndMorePrj
+{
+    public class MainWindowViewModel:ViewModelBase
+    {
+        private RelayCommand _myDialogCmd;
+        public RelayCommand MyDialogCmd => _myDialogCmd ?? (_myDialogCmd = new RelayCommand(
+            () =>  myDialogCmd(),
+            () => { return 1 == 1; },
+			keepTargetAlive:true
+            ));
+        private void myDialogCmd()
+        {
+            MyDialog myDialog = new MyDialog();
+            myDialog.ShowDialog();
+        }
+    }
+}
---
+++ RoutedCommandsAndMorePrj/MyDialog.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace RoutedCommandsAndMorePrj
+{
+    /// <summary>
+    /// Interaction logic for MyDialog.xaml
+    /// </summary>
+    public partial class MyDialog : Window
+    {
+
+
+        public static int GetMyAPint(DependencyObject obj)
+        { return (int)obj.GetValue(MyAPintProperty); }
+        public static void SetMyAPint(DependencyObject obj, int value)
+        { obj.SetValue(MyAPintProperty, value); }
+        public static readonly DependencyProperty MyAPintProperty =
+            DependencyProperty.RegisterAttached("MyAPint", typeof(int), typeof(MyDialog), new PropertyMetadata(0));
+
+
+        public MyDialog()
+        {
+            InitializeComponent();
+        }
+
+        private void MyCC_Click(object sender, RoutedEventArgs e)
+        {
+            if (!(e.Source is MyUC myUC))
+                return;
+            string message = $"{myUC.GetValue(MyAPintProperty) }";
+            MessageBox.Show(message);
+        }
+
+    }
+}
