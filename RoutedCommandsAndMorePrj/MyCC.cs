ab0c290e3bfa49f85593cc14f53731ecb2e48e9b
Agatino-P <57068089+Agatino-P@users.noreply.github.com>
2020-10-11 18:07:08 +0200

    Working on ICommandSource

--- RoutedCommandsAndMorePrj/MyCC.cs
+++ RoutedCommandsAndMorePrj/MyCC.cs
@@ -14,11 +14,64 @@ using System.Windows.Shapes;
 
 namespace RoutedCommandsAndMorePrj
 {
-    public class MyCC : Control
+    public class MyCC : Control, ICommandSource
     {
+        private Ellipse _ellipse;
         static MyCC()
         {
             DefaultStyleKeyProperty.OverrideMetadata(typeof(MyCC), new FrameworkPropertyMetadata(typeof(MyCC)));
         }
+
+        private EventHandler _canExecuteChangedHandler;
+
+        public ICommand Command
+        {
+            get { return (ICommand)GetValue(CommandProperty); }
+            set { SetValue(CommandProperty, value); }
+        }
+        public static readonly DependencyProperty CommandProperty =
+            DependencyProperty.Register("Command", typeof(ICommand), typeof(MyCC), new PropertyMetadata(null));
+
+
+
+        public Object CommandParameter
+        {
+            get { return (Object)GetValue(CommandParameterProperty); }
+            set { SetValue(CommandParameterProperty, value); }
+        }
+        public static readonly DependencyProperty CommandParameterProperty =
+            DependencyProperty.Register("CommandParameter", typeof(Object), typeof(MyCC), new PropertyMetadata(null));
+
+
+
+        public IInputElement CommandTarget
+        {
+            get { return (IInputElement)GetValue(CommandTargetProperty); }
+            set { SetValue(CommandTargetProperty, value); }
+        }
+        public static readonly DependencyProperty CommandTargetProperty =
+            DependencyProperty.Register("CommandTarget", typeof(IInputElement), typeof(MyCC), new PropertyMetadata(null));
+
+        public override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+            if (_ellipse != null)
+            {
+                _ellipse.MouseLeftButtonUp -= Ellipse_MouseLeftButtonUp;
+            }
+            _ellipse = GetTemplateChild("PART_Ellipse") as Ellipse;
+            if (_ellipse != null)
+            {
+                _ellipse.MouseLeftButtonUp += Ellipse_MouseLeftButtonUp;
+            }
+        }
+
+        private void Ellipse_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            
+        }
+
+        public class 
+
     }
 }
